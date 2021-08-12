de657d60e476599256b9d9e66c46924734de7974
谈亚健 <tanyajian@mincang.net>
2021-08-12 17:47:32 +0800

    init

---
+++ JPBaseExtention/JPBaseExtention/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JPBaseExtention
+//
+//  Created by YJ-T on 2021/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
